ce32e6314e3ea593e124f3e19e5c1c077fff011b
neurolius <ewgensafonow@gmail.com>
2026-03-11 12:15:13 +0800

    Создан класс Vector и основные методы для него

--- Form1.cs
+++ Form1.cs
@@ -12,4 +12,40 @@ namespace TaskForYourType
 
         }
     }
+
+    public class Vector
+    {
+        private int X, Y, Z;
+        public Vector(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Vector Sum(Vector v) { 
+            Vector result = new Vector(this.X+v.X, this.Y+v.Y, this.Z+v.Z);
+            return result;
+        }
+
+        public Vector Sub(Vector v) {
+            Vector result = new Vector(this.X-v.X, this.Y-v.Y, this.Z-v.Z);
+            return result;
+        }
+
+        public int Dot(Vector v) { 
+            int result = this.X*v.X + this.Y+v.Y + this.Z*v.Z;
+            return result;
+            
+        }
+
+        public double Lenght() { 
+            double result = Math.Sqrt(X*X + Y*Y + Z + Z);
+            return result;
+        }
+
+        public Vector Cross(Vector v) {
+            Vector result = new Vector(this.Y * v.Z - this.Z * v.Y, this.Z*v.X - this.X*v.Z, this.X*v.Y - this.Y*v.X);
+            return result;
+        }
+    }
 }
